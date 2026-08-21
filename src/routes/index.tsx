1fd7c20e69e3b8679809d0aeac3f2dd428ca3f96
Gianna Rivera <gigirunner2004@gmail.com>
2026-08-21 16:42:49 -0400

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,3 @@
-
 import { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useMemo, useState } from "react";
